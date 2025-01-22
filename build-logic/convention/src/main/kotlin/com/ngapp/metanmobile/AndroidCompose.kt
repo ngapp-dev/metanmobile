b736aa0af7dc0bdc5af920ab890cb0125aafcc99
ngapps <ngapps.dev@gmail.com>
2025-01-22 01:58:55 +0100

    Update gradle and plugins

--- build-logic/convention/src/main/kotlin/com/ngapp/metanmobile/AndroidCompose.kt
+++ build-logic/convention/src/main/kotlin/com/ngapp/metanmobile/AndroidCompose.kt
@@ -20,7 +20,6 @@ package com.ngapp.metanmobile
 import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.Project
 import org.gradle.api.provider.Provider
-import org.gradle.kotlin.dsl.assign
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
 import org.jetbrains.kotlin.compose.compiler.gradle.ComposeCompilerGradlePluginExtension
